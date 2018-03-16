dc3b950738044be64e7ded73f4273fb4122b8e1b
DSBD <baciudavid.dragos@gmail.com>
2018-03-15 22:18:47 -0400

    Add files via upload

---
+++ EminExample/EminExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EminExample
+{
+    public enum Temprature
+    {
+        low = -20 ,
+        medium = 5,
+        high = 30
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Temprature temp;
+            temp = Temprature.low;
+            if (temp == Temprature.high)
+            {
+
+            }
+        }
+    }
+}
