c89d5b00623508fcda0c9453e488063d7feadbd6
DSBD <baciudavid.dragos@gmail.com>
2018-03-15 22:15:13 -0400

    Add files via upload

---
+++ CSharpPlayground/CSharpPlayground/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 12;
+            System.Console.WriteLine("OG X: " + x);
+
+            int y = x;
+            y = 30;
+
+            System.Console.WriteLine("Y: " + y);
+            System.Console.WriteLine("X: " + x);
+
+            System.Console.Write("\nPress [ENTER] to Continue  ");
+            System.Console.ReadLine();
+        }
+    }
+}
