7420e4e08d8b28c5e08914165f5fefde78689470
DSBD <baciudavid.dragos@gmail.com>
2018-03-15 22:19:08 -0400

    Add files via upload

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = getNumber("Enter Your Age: ");
+
+            Console.WriteLine($"Your Age is {number}");
+            Console.ReadLine();
+        }
+
+        static int getNumber(string message)
+        {
+            int number;
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write(message);
+                    number = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Input Error");
+                }
+            }
+            return number;
+        }
+    }
+}
