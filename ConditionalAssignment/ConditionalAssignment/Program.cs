06ab9b587f91d03ec353cc0a623bfcad87f6cb79
DSBD <baciudavid.dragos@gmail.com>
2018-03-15 22:14:24 -0400

    Add files via upload

---
+++ ConditionalAssignment/ConditionalAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConditionalAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = 5;
+
+            price = price >= 10 ? 9.50 : 10.00;
+        }
+    }
+}
